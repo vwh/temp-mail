f29635aa1b815e11d978ea3ea2ba6098a22eeba6
Yazan Alqasem <vwhe@proton.me>
2025-07-17 20:36:51 +0300

    feat: added zod validatior middleware

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -1,14 +1,16 @@
-import type { Context } from "hono";
-
+import type { Handler } from "hono";
 import * as db from "@/db";
 import type { Email, EmailSummary } from "@/schemas/emailSchema";
 
-type Bindings = { Bindings: CloudflareBindings };
+type Env = {
+	Bindings: CloudflareBindings;
+};
 
-export async function getEmails(c: Context<Bindings>) {
+export const getEmails: Handler<Env> = async (c) => {
 	const { DB } = c.env;
-
+	
 	const emailAddress = c.req.param("emailAddress");
+	
 	const limit = Number(c.req.query("limit")) || 10;
 	const offset = Number(c.req.query("offset")) || 0;
 
@@ -20,11 +22,11 @@ export async function getEmails(c: Context<Bindings>) {
 	)) as EmailSummary[];
 
 	return c.json(results);
-}
+};
 
-export async function getEmailById(c: Context<Bindings>) {
+export const getEmailById: Handler<Env> = async (c) => {
 	const { DB } = c.env;
-
+	
 	const emailId = c.req.param("emailId");
 
 	const result = (await db.getEmailById(DB, emailId)) as Email | null;
@@ -34,24 +36,28 @@ export async function getEmailById(c: Context<Bindings>) {
 	}
 
 	return c.json(result);
-}
+};
 
-export async function deleteEmailsByAddress(c: Context<Bindings>) {
+export const deleteEmailsByAddress: Handler<Env> = async (c) => {
 	const { DB } = c.env;
-
+	
 	const emailAddress = c.req.param("emailAddress");
 
 	await db.deleteEmailsByRecipient(DB, emailAddress);
 
-	return c.json({ message: `Emails for ${emailAddress} deleted successfully.` });
-}
+	return c.json({ 
+		message: `Emails for ${emailAddress} deleted successfully.` 
+	});
+};
 
-export async function deleteEmailById(c: Context<Bindings>) {
+export const deleteEmailById: Handler<Env> = async (c) => {
 	const { DB } = c.env;
-
+	
 	const emailId = c.req.param("emailId");
 
 	await db.deleteEmailById(DB, emailId);
 
-	return c.json({ message: `Email with ID ${emailId} deleted successfully.` });
-}
+	return c.json({ 
+		message: `Email with ID ${emailId} deleted successfully.` 
+	});
+};
--- src/routes/emailRoutes.ts
+++ src/routes/emailRoutes.ts
@@ -6,11 +6,35 @@ import {
 	getEmails,
 } from "@/controllers/emailController";
 
+import { zValidator } from '@hono/zod-validator';
+import { emailAddressParamSchema, emailIdParamSchema, emailQuerySchema } from "@/schemas/emailRouterSchema";
+
+
 const emailRoutes = new Hono<{ Bindings: CloudflareBindings }>();
 
-emailRoutes.get("/emails/:emailAddress", getEmails);
-emailRoutes.delete("/emails/:emailAddress", deleteEmailsByAddress);
-emailRoutes.get("/inbox/:emailId", getEmailById);
-emailRoutes.delete("/inbox/:emailId", deleteEmailById);
+emailRoutes.get(
+	"/emails/:emailAddress",
+	zValidator("param", emailAddressParamSchema),
+	zValidator("query", emailQuerySchema),
+	getEmails
+);
+
+emailRoutes.delete(
+	"/emails/:emailAddress",
+	zValidator("param", emailAddressParamSchema),
+	deleteEmailsByAddress
+);
+
+emailRoutes.get(
+	"/inbox/:emailId",
+	zValidator("param", emailIdParamSchema),
+	getEmailById
+);
+
+emailRoutes.delete(
+	"/inbox/:emailId",
+	zValidator("param", emailIdParamSchema),
+	deleteEmailById
+);
 
 export default emailRoutes;
