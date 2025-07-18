4f69c6b5e461a232219ef95d129fdf6bb4951f8c
Yazan Alqasem <vwhe@proton.me>
2025-07-18 16:58:43 +0300

    feat: new domain

--- src/domains.ts
+++ src/domains.ts
@@ -1,2 +1,2 @@
 // List of supported email domains
-export const DOMAINS = ["iusearch.lol", "lifetalk.us", "barid.site", "z44d.pro"];
+export const DOMAINS = ["iusearch.lol", "lifetalk.us", "barid.site", "z44d.pro", "vwh.sh"];
