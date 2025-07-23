06b14349472453b8061c0bd6e7abaf6197ced55f
Yazan Alqasem <vwhe@proton.me>
2025-07-23 22:58:59 +0300

    chore: make delete time 4 hours

--- src/handlers/scheduledHandler.ts
+++ src/handlers/scheduledHandler.ts
@@ -2,18 +2,18 @@ import * as db from "@/database/queries";
 import { now } from "@/utils/date";
 import { throwError } from "@/utils/error";
 
-const HOURS_TO_DELETE = 8;
+const HOURS_TO_DELETE = 4;
 
 /**
  * Cloudflare Scheduled Function
- * Delete emails older than 8 hours
+ * Delete emails older than 4 hours
  */
 export async function handleScheduled(
 	_event: ScheduledEvent,
 	env: CloudflareBindings,
 	_ctx: ExecutionContext,
 ) {
-	const cutoffTimestamp = now() - HOURS_TO_DELETE * 60 * 60; // 8 hours ago in seconds
+	const cutoffTimestamp = now() - HOURS_TO_DELETE * 60 * 60; // 4 hours ago in seconds
 
 	// Delete old emails
 	const { success, error } = await db.deleteOldEmails(env.DB, cutoffTimestamp);
