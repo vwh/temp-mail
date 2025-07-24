739f97e7ac05f235daa9fc4e3dff09ce6ff67ada
Yazan Alqasem <vwhe@proton.me>
2025-07-24 09:53:02 +0300

    fix: update cron schedule for scheduled functions

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ export default {
 	// Cloudflare Scheduled Functions
 	scheduled: (event: ScheduledEvent, env: CloudflareBindings, ctx: ExecutionContext) => {
 		switch (event.cron) {
-			case "0 */5 * * *":
+			case "0 */4 * * *":
 				return handleScheduled(event, env, ctx);
-			case "0 0 * * *":
+			case "0 */5 * * *":
 				return handleDailyReport(event, env, ctx);
 		}
 	},
